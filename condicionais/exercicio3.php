29315f44f8f8cf3aff73581a1b05f15132037c87
Pedro Lucas <izielucas2016@gmail.com>
2025-03-10 09:43:30 -0300

    FEAT/Added form html to exercise

--- condicionais/exercicio3.php
+++ condicionais/exercicio3.php
@@ -1,7 +1,14 @@
+<html>
+    <body>
+        <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
+            Valor a Sacar: <input type="text" name="saque" id="saque">
+            <input type="submit" value="Sacar">
+        </form>
+    </body>
+</html>
+
 <?php 
 //Em um determinado momento do dia, apenas notas de 10, 50 e 100 estão disponíveis em um terminal de caixa eletrônico. Elaborar um programa que leia um valor de saque de um cliente, verifique sua validade (ou seja, se pode ser pago com as notas disponíveis) e informe o número mínimo de notas de 100,50,10.
-
-$valorASacar = 100;
 
 function saque($valor){
     if($valor%100 ==0 || $valor%50==0 || $valor%10==0){
@@ -13,9 +20,12 @@ function saque($valor){
         $ced10 = (int)$ced10;
         return "O minímo de cédulas para este saque é de: <br>Cedúlas de R100: $ced100<br>Cedúlas de R50: $ced50<br>Cedúlas de R10: $ced10";
     }else{
-        return "Não será possivél realizar o saque de $valor!";
+        return "Não será possivél realizar o saque de $valor";
     }
 }
 
-echo saque($valorASacar);
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $valorASacar = $_POST["saque"];
+    echo saque($valorASacar);
+}
 ?>
