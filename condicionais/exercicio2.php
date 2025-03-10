4818964401c1ed1e99fa1aa24c1b1c464af9011c
Pedro Lucas <izielucas2016@gmail.com>
2025-03-10 09:37:55 -0300

    FEAT/Added form html to exercise

--- condicionais/exercicio2.php
+++ condicionais/exercicio2.php
@@ -1,3 +1,12 @@
+<html>
+    <body>
+        <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
+            Raiz: <input type="text" name="fraiz" id="fraiz">
+            <input type="submit" value="Enviar">
+        </form>
+    </body>
+</html>
+
 <?php
 //Elaborar um programa que leia um número e calcule sua raiz quadrada. Caso a raiz seja exata (quadrados perfeitos), informá-la, caso contrário, informe: ‘Não há raiz exata para ..’
 
@@ -12,5 +21,12 @@ function raiz($numero){
     }
 }
 
-echo raiz(4)
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $raiz = $_POST['fraiz'];
+    if(empty($raiz)){
+        echo "A raiz não pode ser vazia!";
+    }else{
+        echo raiz($raiz);
+    }    
+}
 ?>
