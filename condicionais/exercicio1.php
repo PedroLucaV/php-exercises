7f1b7fda22ab6213fc5adce0b2dd031b8b5bd799
Pedro Lucas <izielucas2016@gmail.com>
2025-03-10 09:26:45 -0300

    FEAT/Added form html to exercise

--- condicionais/exercicio1.php
+++ condicionais/exercicio1.php
@@ -1,7 +1,14 @@
+<html>
+    <body>
+        <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
+            Hora Brasil: <input type="text" name="horaBrasil" id="horaBrasil">
+            <input type="submit" value="Enviar">
+        </form>
+    </body>
+</html>
+
 <?php
 //Sabendo que o fuso horário da França em relação ao Brasil é de + 5 horas (no horário de verão na França), elaborar um programa que leia a hora no Brasil e informe a hora na França.
-
-$horaBrasil = 22;
 
 function horaBrazilFranca($hora){
     $horaFranca = $hora+5;
@@ -14,7 +21,12 @@ function horaBrazilFranca($hora){
 
     return $horaFranca;
 }
-
-echo horaBrazilFranca($horaBrasil);
-
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $hora_brasil = $_POST['horaBrasil'];
+    if(empty($hora_brasil)){
+        echo "O Valor não pode ser vazio!";
+    }else{
+        echo "$hora_brasil horas no Brasil são: ".horaBrazilFranca($hora_brasil)." horas na França";
+    }    
+}
 ?>
